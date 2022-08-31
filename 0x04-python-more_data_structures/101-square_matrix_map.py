db79aa5756ad6115b49203b58ced788ef7b56843
Msjudy <sigalajnicole@gmail.com>
2022-08-31 16:05:33 -0700

    Task 14 py

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,10 +1,3 @@
 #!/usr/bin/python3
-def weight_average(my_list=[]):
-    if len(my_list) == 0:
-        return 0
-    sum_score = 0
-    sum_weight = 0
-    for i in my_list:
-        sum_score += i[0] * i[1]
-        sum_weight += i[1]
-    return sum_score / sum_weight
+def square_matrix_map(matrix=[]):
+    return list(map(lambda x: list(map(lambda y: y**2, x)), matrix))
