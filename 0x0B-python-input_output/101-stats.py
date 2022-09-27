cc1d15181b405b37a56d4a7b97a2657b9da502e4
Msjudy <sigalajnicole@gmail.com>
2022-09-27 14:49:36 -0700

    Task 14

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,51 +1,15 @@
 #!/usr/bin/python3
-"""
-Reads from standard input and computes metrics
-"""
+import random
+import sys
+from time import sleep
+import datetime
 
-
-if __name__ == "__main__":
-    import sys
-
-    stdin = sys.stdin
-
-    c = 0
-    size = 0
-    vd = ['200', '301', '400', '401', '403', '404', '405', '500']
-    st = {}
-
-    try:
-        for line in stdin:
-            if c == 10:
-                print("File size: {}".format(size))
-                for i in sorted(st):
-                    print("{}: {}".format(i, st[i]))
-                c = 1
-            else:
-                c = c + 1
-
-            line = line.split()
-
-            try:
-                size = size + int(line[-1])
-            except (IndexError, ValueError):
-                pass
-
-            try:
-                if line[-2] in vd:
-                    if st.get(line[-2], -1) == -1:
-                        st[line[-2]] = 1
-                    else:
-                        st[line[-2]] = st[line[-2]] + 1
-            except IndexError:
-                pass
-
-        print("File size: {}".format(size))
-        for i in sorted(st):
-            print("{}: {}".format(i, st[i]))
-
-    except KeyboardInterrupt:
-        print("File size: {}".format(size))
-        for i in sorted(st):
-            print("{}: {}".format(i, st[i]))
-        raise
+for i in range(10000):
+    sleep(random.random())
+    sys.stdout.write("{:d}.{:d}.{:d}.{:d} - [{}] \"GET /projects/260 HTTP/1.1\" {} {}\n".format(
+        random.randint(1, 255), random.randint(1, 255), random.randint(1, 255), random.randint(1, 255),
+        datetime.datetime.now(),
+        random.choice([200, 301, 400, 401, 403, 404, 405, 500]),
+        random.randint(1, 1024)
+    ))
+    sys.stdout.flush()
